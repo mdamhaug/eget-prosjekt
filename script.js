e32463b3a70c7d4c979b9fa1cfccecaf4da1a95e
mdamhaug <mariadamhaug@gmail.com>
2026-05-07 13:58:18 +0200

    ny

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ closeEl.addEventListener("click", function () {
     burgerEl.classList.toggle("active")
     closeEl.classList.toggle("active")
 })
+
+const shoeImgEl = document.querySelector(".shoe-img")
+const shoeImg2El = document.querySelector(".shoe-img2")
+
+shoeImgEl.addEventListener("mouseover", function () {
+    shoeImgEl.classList.toggle("active")
+    shoeImg2El.classList.toggle("active")
+})
+
+shoeImg2El.addEventListener("mouseout", function () {
+    shoeImgEl.classList.toggle("active")
+    shoeImg2El.classList.toggle("active")
+})
+
+const shopBtnEls = document.querySelectorAll(".shop-btn")
+const bascetEl = document.querySelector(".bascet")
+
+for (const shopBtnEl of shopBtnEls) {
+    shopBtnEl.addEventListener("click", function () {
+        bascetEl.classList.add("active")
+    })
+
+}
